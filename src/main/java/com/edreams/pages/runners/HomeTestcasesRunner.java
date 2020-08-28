5251b46b8d421bc954d6b0580cb9c723b4ea0633
Govardhan Karnati <gkarnati-c@g2llc.com>
2020-08-28 22:13:59 +0530

    Moved files from src/test/java1 to src/test/java

--- src/main/java/com/edreams/pages/runners/HomeTestcasesRunner.java
+++ src/main/java/com/edreams/pages/runners/HomeTestcasesRunner.java
@@ -3,12 +3,12 @@ package com.edreams.pages.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features="src/test/java1/com/edreams/features/home",							
+@CucumberOptions(features="src/test/java/com/edreams/features/home",							
 					 glue="com.edreams.testcases",
 					 plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
 							 "junit:target/cucumber-reports/Cucumber.xml",
 							 "html:target/cucumber-reports/cucumber"
-					 } )					
+					 })					
 public class HomeTestcasesRunner extends AbstractTestNGCucumberTests{
 
 }
