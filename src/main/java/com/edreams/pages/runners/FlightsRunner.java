ce9b302f7fa5e1a1d62f81c5aab0235bf3f368a0
Basheernavan <basheerdublin@gmail.com>
2020-09-03 18:12:02 +0100

    1. Created mastersuite and executed from commandline

--- src/main/java/com/edreams/pages/runners/FlightsRunner.java
+++ src/main/java/com/edreams/pages/runners/FlightsRunner.java
@@ -3,7 +3,7 @@ package com.edreams.pages.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features="src/test/java/com/edreams/features/flights/returnflights.feature",							
+@CucumberOptions(features="src/test/java/com/edreams/features/flights",							
 					 glue="com.edreams.testcases.stepdefinitions",
 					 plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
 							 "junit:target/cucumber-reports/Cucumber.xml",
